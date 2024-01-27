4788a29218b20e19514e972b0ea70385600489cc
schip454 <118295365+schip454@users.noreply.github.com>
2024-01-27 16:26:09 +0500

    vercel bug fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,9 +2,9 @@ import { FC } from 'react';
 import Header from '../Header/Header';
 import AppRoutes from './AppRoutes';
 import Footer from '../Footer/Footer';
+import PlayBar from '../PlayBar/PlayBar';
 
 import '../../scss/main.scss';
-import PlayBar from '../PlayBar/PlayBar';
 
 const App: FC = () => {
   return (
